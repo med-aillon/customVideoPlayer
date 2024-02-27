34c9e59fba06c53897e266cef6060be7b55d857d
med-aillon <mehdimerghichi@gmail.com>
2024-02-27 20:11:20 +0100

    Gérer la range et le jeu de la vidéo

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ function fillDurationVariables() {
 
   formatValue(current, timersDisplay[0]);
   formatValue(totalDuration, timersDisplay[1]);
+
+  video.removeEventListener("loadeddata", fillDurationVariables);
+  window.removeEventListener("load", fillDurationVariables);
 }
 
 function formatValue(val, el) {
@@ -42,4 +45,48 @@ function formatValue(val, el) {
   }
 
   el.textContent = `${currentMin}:${currentSec}`;
+}
+
+const progress = document.querySelector(".progress");
+
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+function handleTimeUpdate() {
+  current = video.currentTime;
+
+  formatValue(current, timersDisplay[0]);
+  const progressPosition = current / totalDuration;
+  progress.style.transform = `scaleX(${progressPosition})`;
+  if (video.ended) {
+    togglerImg.src = "ressources/play.svg";
+  }
+}
+
+const muteBtn = document.querySelector(".mute-btn");
+const muteImg = document.querySelector(".mute-btn img");
+
+muteBtn.addEventListener("click", handleMute);
+
+function handleMute() {
+  if (!video.muted) {
+    video.muted = true;
+    muteImg.src = "ressources/mute.svg";
+  } else {
+    video.muted = false;
+    muteImg.src = "ressources/unmute.svg";
+  }
+}
+
+const volumeSlider = document.querySelector(".volume-slider");
+
+volumeSlider.addEventListener("change", handleVolumeModification);
+
+function handleVolumeModification() {
+  video.volume = volumeSlider.value / 100;
+
+  if (video.volume === 0) {
+    muteImg.src = "ressources/mute.svg";
+  } else {
+    muteImg.src = "ressources/unmute.svg";
+  }
 }
